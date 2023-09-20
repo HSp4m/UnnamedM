f125fcb187cf8deccc0347292a16ea1e44a36ed6
HSp4m <sp4m787@gmail.com>
2023-09-20 18:14:52 -0400

    0.1.1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Unnamed 0.1.0.1
+# Unnamed 0.1.1
 
 A menu in python just for fun :) (This script is only visual.)
--- unnamed.py
+++ unnamed.py
@@ -71,7 +71,7 @@ url = 'https://raw.githubusercontent.com/HSp4m/UnnamedM/main/version.txt'
 FileURL = 'https://raw.githubusercontent.com/HSp4m/UnnamedM/main/unnamed.py'
 
 page = urllib.request.urlopen(url)
-c_version = "0.1.0.1"
+c_version = "0.1.1"
 u_version = f"{page.read()}".replace("b","").replace("'","").replace("n","").replace("\\","")
 p_enabled = False;
 
